9a8c750eb5628a9834858cc9d8a72cd0f0bf7ab3
konyomba <konyomba@gmail.com>
2023-09-07 07:43:46 -0700

    6size.c file commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	float l;
 	long int m;
 	long long int d;
-	printf("Size of a long int: %lu.\n",(unsigned long)sizeof(m));
-	printf("Size of a long long int: %lu.\n",(unsigned long)sizeof(d));
-	printf("Size of a int: %lu.\n",(unsigned long)sizeof(k));
-	printf("Size of a char: %lu.\n",(unsigned long)sizeof(c));
-	printf("Size of a float: %lu.\n",(unsigned long)sizeof(l));
+	printf("Size of a long int: %lu  byte(s)\n",(unsigned long)sizeof(m));
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
+	printf("Size of a int: %lu byte(s)\n",(unsigned long)sizeof(k));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(l));
 	return (0);
 }
