75547a844d08312794c4c2d164e3a6cd1699ade4
konyomba <konyomba@gmail.com>
2023-09-07 07:34:59 -0700

    6size.c file commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - programm that prints the sizes of various data types
  *
- * Return: 0 (terminate)
+ * Return 0 (terminate)
  */
 
 int main(void)
@@ -10,7 +10,11 @@ int main(void)
 	int k;
 	char c;
 	float l;
-	printf("Size of a long long int: %lu.\n",(unsigned long)sizeof(k));
+	long int m;
+	long long int d;
+	printf("Size of a long int: %lu.\n",(unsigned long)sizeof(m));
+	printf("Size of a long long int: %lu.\n",(unsigned long)sizeof(d));
+	printf("Size of a int: %lu.\n",(unsigned long)sizeof(k));
 	printf("Size of a char: %lu.\n",(unsigned long)sizeof(c));
 	printf("Size of a float: %lu.\n",(unsigned long)sizeof(l));
 	return (0);
