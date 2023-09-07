6851814252c36b614c37659de18092898dca206a
konyomba <konyomba@gmail.com>
2023-09-07 06:48:06 -0700

    first c programm commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point to the programm
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
