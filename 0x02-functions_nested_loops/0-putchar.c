51df428bb31d8c8a1ec10e41ac6b5f7eeb533e34
konyomba <konyomba@gmail.com>
2023-09-12 09:05:48 -0700

    0-putchar.c file commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - a c program that prints to the standard output
+ * Return: 0 (after compilation)
+ *
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return	(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The directory contains c programs that illuustrate the concept of functions and nested loops.The first file of the directory is 0-putchar.c which is a file program that prints "_putchar" followed by a new line.
