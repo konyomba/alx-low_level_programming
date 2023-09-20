82cc3c284f5c08f1d21e52483ff2cd245ba9a86a
konyomba <konyomba@gmail.com>
2023-09-20 02:03:09 -0700

    0-strcat.c file commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - joins two strings
+ * @dest: first string
+ * @src: second string
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int k;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	while (src[k] != '\0')
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
