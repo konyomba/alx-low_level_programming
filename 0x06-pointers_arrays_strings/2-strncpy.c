1c5cebb4be4869ce1294cad79b686be7a2b0439a
konyomba <konyomba@gmail.com>
2023-09-21 01:49:11 -0700

    2-strncpy.c file commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,21 +9,20 @@
  * Return: dest
  */
 
-char * _strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+		int j;
 
-	i = 0;
-	while (i < &&src[i] != '\0')
-	{
-		dest[i] = src[i];
-			i++;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-
-	return (dest);
+		j = 0;
+		while (j < n && src[j] != '\0')
+		{
+			dest[j] = src[j];
+			j++;
+		}
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+		return (dest);
 }
