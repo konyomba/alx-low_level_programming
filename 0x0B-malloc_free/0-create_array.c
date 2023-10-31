4c57e5128e5d9ebb20aa26aed7a7a65699669a7a
konyomba <konyomba@gmail.com>
2023-10-31 08:26:27 -0700

    task-0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	array = malloc(sizeof(char) * size);
 	if (size == 0)
 		return (NULL);
+	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
