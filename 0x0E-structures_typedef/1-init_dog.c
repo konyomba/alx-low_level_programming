db1af03d190c3707e868ebf2f43552a0c8c20c27
konyomba <konyomba@gmail.com>
2023-11-07 08:56:59 -0800

    final

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,7 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {	
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
-		return (NULL);
+		return;
 	{
 	/**dereferencing**/
 	d->name = name;
