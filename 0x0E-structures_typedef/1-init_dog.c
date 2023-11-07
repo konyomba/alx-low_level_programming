291b303c160b144c6538571f3e96f13b0b6229a7
konyomba <konyomba@gmail.com>
2023-11-07 08:45:41 -0800

    task-1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * main - function that initializes a struct
+ * init_dog - struct
+ * @name: input
+ * @age: input
+ * @owner: input
+ * @d: dog initialized
+ * Return: 0 on success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	/**dereferencing**/
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
