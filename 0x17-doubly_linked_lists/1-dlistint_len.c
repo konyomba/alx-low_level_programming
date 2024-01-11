94d8110124a59528e29d6ddcd7845b074d63cc28
konyomba <konyomba@gmail.com>
2024-01-11 06:04:37 -0800

    task2

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,12 +7,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-        int count;
+	int count;
 
-        count = 0;
+	count = 0;
 
-        if (h == NULL)
-                return (count);
+	if (h == NULL)
+		return (count);
 
         while (h->prev != NULL)
                 h = h->prev;
@@ -25,4 +25,3 @@ size_t dlistint_len(const dlistint_t *h)
 
         return (count);
 }
-
